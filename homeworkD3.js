6fc2144a84be72d271b43a44944b00d93b4f0517
Bogdan Birau <bdog_20062000@yahoo.com>
2021-08-15 10:28:33 +0100

    Extra Exercise2

--- homeworkD3.js
+++ homeworkD3.js
@@ -6,8 +6,13 @@ ASSIGNMENT RULES
 - You can Google / use StackOverflow BUT we suggest you to use just the material provided
 - You can test your code in a separate file or de-commenting the single exercises in this one.
 You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
-
 */
+
+const writeHeader = function(title) {
+  console.log(`\n\n]>-------------<[ ${title.toUpperCase()} ]>-------------<[`)
+}
+
+
 /* EXERCISE 1
 Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
 */
@@ -29,6 +34,7 @@ Try to describe what a variable is, in your own words.
 /* EXERCISE 3
 Write the code to execute an addition (a sum) of the numbers 12 and 20.
 */
+writeHeader("Exercise 3")
 
 let sum = 12 + 20
 console.log(sum)
@@ -36,6 +42,7 @@ console.log(sum)
 /* EXERCISE 4
 Create a variable named x containing the number 12.
 */
+writeHeader("Exercise 4")
 
 let x = 12
 console.log(x)
@@ -43,13 +50,15 @@ console.log(x)
 /* EXERCISE 5
 Create a variable called name containing the string John Doe.
 */
+writeHeader("Exercise 5")
 
-let name = "John Doe"
+let name = `John Doe`
 console.log(name)
 
 /* EXERCISE 6
 Execute a subtraction between the number 12 and the variable x, which is storing the value 12.
 */
+writeHeader("Exercise 6")
 
 let y = 12 - x
 console.log(y)
@@ -60,24 +69,44 @@ Verify that name1 is different from name2.
 Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2). ?
 */
 
+writeHeader("Exercise 7")
+
 let name1 ="john"
-let name2 ="John"
-if(name1 === name2)
+let name2 ="Bogdan"
+if(name1 !== name2)
 console.log("Name1 is different from Name2")
+console.log(name1.toLocaleLowerCase() === name2.toLocaleLowerCase())
 
 /* EXERCISE 8
 Create a variable named x (its value must be less than 10). Write the code to print the literal value of x (ex.: if x is 1 print "one", if 5 print "five" etc.). +?
 */
 
-x = 9
-console.log(x)
-if(x=9){
-    console.log("Nine")
+writeHeader("Exercise 8")
+
+x = 12
+
+switch(x){
+  case 1 : console.log("one"); break;
+  case 2 : console.log("two"); break;
+  case 3 : console.log("three"); break;
+  case 4 : console.log("four"); break;
+  case 5 : console.log("five"); break;
+  case 6 : console.log("six"); break;
+  case 7 : console.log("seven"); break;
+  case 8 : console.log("eight"); break;
+  case 9 : console.log("nine"); break;
+  default : console.log(`This works only for ${x} < 10`)
 }
+
+
 
 /* EXERCISE 9
 [Extra] Insert a value in a variable based on the resut of a ternary if (topic not covered during lesson, try to search it by yourself, tomorrow morning we'll discuss it together ;) ) ?
 */
+writeHeader("Exercise 9")
+
+let ternaryExample = x > 10 ? true : false
+console.log(`X is ${ternaryExample} and is greater than 10 `)
 
 
 
@@ -86,14 +115,19 @@ if(x=9){
 /*
  1. Use ternary operator to assign "male" or "female" string to a variable based on the value of another 'isMale' variable
 */
+writeHeader("Extra Exercise 1")
+isMale = 2
 
-
-
+let maleOrFemale = x === 1 ? "male" : "female" 
+console.log(`${maleOrFemale}`)
 
 
 /*
  2. Write code to check from two given integers whether one of them is 8 or their sum or difference is 8.  +
 */
+
+writeHeader("Extra Exercise 2")
+
 x = 4
 y = 4
 
@@ -108,19 +142,47 @@ if (x === 8 || y === 8) {
 /*
   3. Create a variable which concatenates two strings. +
 */
+writeHeader("Extra Exercise 2")
 
-let concat = name1 + name2
+let concat = name1 + " " + name2
 console.log(concat)
 
 /*
 4.Write a JavaScript conditional statement to sort three numbers. Display the result in the console. ?
 */
+writeHeader("Extra Exercise 2")
 
+ x = 3
+ y = 6
+let z = 8
+
+if (x > y && x > z){
+  if(y > z){
+    console.log(x + `, ` + y + `, ` + z)
+  }else{
+    console.log(x + `, ` + z + `, ` + y)
+  }
+}else if(y > x && y > z){
+  if (x > z) {
+    console.log(y + `, ` + x + `, ` + z)
+  } else {
+    console.log(y + `, ` + z + `, ` + x)
+  }
+}else if (z > x && z > y){
+  if (x > y) {
+    console.log(z + `, ` + x + `, ` + y)
+  } else {
+    console.log(z + `, ` + y + `, ` + x)
+  }
+}
 
 
 /*
  5. Write code to find the average of two given integers. +
 */
+
+writeHeader("Extra Exercise 3")
+
 
 let average = (x + y) / 2
 console.log("The average is " , average)
@@ -128,6 +190,9 @@ console.log("The average is " , average)
 /* 
   6. Find the longest of two given strings
 */
+writeHeader("Extra Exercise 3")
+
+
 
 /*
  7. Write code to check whether a value is an integer or not. ?
